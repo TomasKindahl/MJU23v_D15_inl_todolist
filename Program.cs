2174371a4f2de40434b3e9cc320c3fdb4f309aea
Ghofranpanah <148060299+Ghofranpanah@users.noreply.github.com>
2025-02-10 14:31:54 +0100

    Uppdatering  va to do list 1,2
    jag har försökt  med olika sätt  engång var det konstroktion  och den andra gången var det argument  och jag inte
     säkert om det programet  funkar som det ska ?

--- Program.cs
+++ Program.cs
@@ -1,301 +1,134 @@
-﻿namespace MJU23v_D15_inl_todolist
+﻿using System;
+using System.Collections.Generic;
+
+public static class MyIO
 {
-    public class Program
+    // Läs inkommando från användaren
+    public static string ReadCommand(string prompt)
     {
-        public static void Main(string[] args)
+        Console.Write(prompt);
+        return Console.ReadLine() ?? string.Empty;
+    }
+
+    // Jämför om kommandot är lika med förväntat kommando
+    public static bool CommandEquals(string input, string expected)
+    {
+        if (string.IsNullOrWhiteSpace(input)) return false;
+        var parts = input.Split(' ', (char)StringSplitOptions.RemoveEmptyEntries);
+        return string.Equals(parts[0], expected, StringComparison.OrdinalIgnoreCase);
+    }
+
+    // Hämta antalet argument i kommandot
+    public static int NumArguments(string input)
+    {
+        return string.IsNullOrWhiteSpace(input) ? 0 : input.Split(' ', (char)StringSplitOptions.RemoveEmptyEntries).Length - 1;
+    }
+
+    // Hämta ett specifikt argument från kommandot
+    public static string Argument(string input, int index)
+    {
+        var parts = input.Split(' ', (char)StringSplitOptions.RemoveEmptyEntries);
+        return (index < parts.Length) ? parts[index] : string.Empty;
+    }
+
+    // Kontrollera om kommandot har ett specifikt argument
+    public static bool HasArgument(string input, string argument)
+    {
+        return !string.IsNullOrWhiteSpace(input) && input.IndexOf(argument, StringComparison.OrdinalIgnoreCase) >= 0;
+    }
+}
+
+public class Todo
+{
+    public enum TodoItem { Active, Ready, Waiting }
+
+    private static Dictionary<int, TodoItem> todoList = new Dictionary<int, TodoItem>();
+
+    public static void ReadListFromFile()
+    {
+        // Simulerar inläsning från en fil
+        todoList[1] = TodoItem.Waiting;
+        todoList[2] = TodoItem.Active;
+        todoList[3] = TodoItem.Ready;
+    }
+
+    public static void PrintHelp()
+    {
+        Console.WriteLine("Kommandon:");
+        Console.WriteLine("hjälp - Visa hjälp");
+        Console.WriteLine("aktivera /nummer/ - Aktivera ett objekt");
+        Console.WriteLine("klar /nummer/ - Markera ett objekt som klart");
+        Console.WriteLine("vänta /nummer/ - Sätt ett objekt på vänt");
+        Console.WriteLine("lista - Visa listan");
+        Console.WriteLine("sluta - Avsluta programmet");
+    }
+
+    public static void TrySetStatus(string argument, TodoItem status)
+    {
+        if (int.TryParse(argument, out int itemId) && todoList.ContainsKey(itemId))
         {
-            Console.WriteLine("Välkommen till att-göra-listan!");
-            Todo.ReadListFromFile();
-            Todo.PrintHelp();
-            string command;
-            do
-            {
-                command = MyIO.ReadCommand("> ");
-                if (MyIO.Equals(command, "hjälp"))
-                {
-                    Todo.PrintHelp();
-                }
-                else if (MyIO.Equals(command, "aktivera"))
-                {
-                    if (MyIO.NumArguments(command) != 1)
-                    {
-                        Console.WriteLine("Användning: aktivera /nummer/");
-                    }
-                    else
-                    {
-                        string arg = MyIO.Argument(command, 0);
-                        Todo.TrySetStatus(arg, Todo.TodoItem.Active);
-                    }
-                }
-                else if (MyIO.Equals(command, "klar"))
-                {
-                    if (MyIO.NumArguments(command) != 1)
-                    {
-                        Console.WriteLine("Användning: klar /nummer/");
-                    }
-                    else
-                    {
-                        string arg = MyIO.Argument(command, 0);
-                        Todo.TrySetStatus(arg, Todo.TodoItem.Ready);
-                    }
-                }
-                else if (MyIO.Equals(command, "vänta"))
-                {
-                    if (MyIO.NumArguments(command) != 1)
-                    {
-                        Console.WriteLine("Användning: vänta /nummer/");
-                    }
-                    else
-                    {
-                        string arg = MyIO.Argument(command, 0);
-                        Todo.TrySetStatus(arg, Todo.TodoItem.Waiting);
-                    }
-                }
-                else if (MyIO.Equals(command, "lista"))
-                {
-                    if (MyIO.HasArgument(command, "allt"))
-                        Todo.PrintTodoList(verbose: true);
-                    else
-                        Todo.PrintTodoList(verbose: false);
-                }
-                else if (MyIO.Equals(command, "sluta"))
-                {
-                    Console.WriteLine("Hej då!");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine($"Okänt kommando: {command}");
-                }
-            }
-            while (true);
+            todoList[itemId] = status;
+            Console.WriteLine($"Objekt {itemId} har nu status: {status}");
+        }
+        else
+        {
+            Console.WriteLine("Fel: Ange ett giltigt nummer som finns i listan.");
         }
     }
-    public class Todo
+
+    public static void PrintTodoList(bool verbose)
     {
-        public static List<TodoItem> list = new List<TodoItem>();
-        public class TodoItem
+        Console.WriteLine("Att-göra-lista:");
+        foreach (var item in todoList)
         {
-            // Constants
-            public const int Active = 1;
-            public const int Waiting = 2;
-            public const int Ready = 3;
-            // TODO: make an enum of the above
-
-            // Attribute
-            public int status;
-            public int priority;
-            public string task;
-            public string taskDescription;
-            public TodoItem(int priority, string task)
-            {
-                this.status = Active;
-                this.priority = priority;
-                this.task = task;
-                this.taskDescription = "";
-            }
-            public TodoItem(string todoLine)
-            {
-                string[] field = todoLine.Split('|');
-                status = Int32.Parse(field[0]);
-                priority = Int32.Parse(field[1]);
-                task = field[2];
-                taskDescription = field[3];
-            }
-            public string ToString(bool verbose = false)
-            {
-                string res = $"|{StatusString(),-12}|{priority,-6}|{task,-20}|";
-                if (verbose)
-                    res += $"{taskDescription,-40}|";
-                return res;
-            }
-            public void Print(bool verbose = false)
-            {
-                Console.WriteLine(ToString());
-            }
-            public bool SetActive()
-            {
-                if (status == Waiting)
-                {
-                    status = Active;
-                    return true;
-                }
-                return false;
-            }
-            public bool SetReady()
-            {
-                if (status == Active)
-                {
-                    status = Ready;
-                    return true;
-                }
-                return false;
-            }
-            public bool SetWaiting()
-            {
-                if (status == Active)
-                {
-                    status = Waiting;
-                    return true;
-                }
-                return false;
-            }
-            public bool SetStatus(int newStatus)
-            {
-                switch(newStatus)
-                {
-                    case Active: return SetActive();
-                    case Waiting: return SetWaiting();
-                    case Ready: return SetReady();
-                    default: return false;
-                }
-            }
-            public string StatusString()
-            {
-                switch (status)
-                {
-                    case Active: return "aktiv";
-                    case Waiting: return "väntande";
-                    case Ready: return "avklarad";
-                    default: return "(felaktig)";
-                }
-            }
-        }
-        public static void ReadListFromFile()
-        {
-            string dir = @"..\..\..\";
-            string todoFileName = $"{dir}todo.lis";
-            Console.Write($"Läser från fil {todoFileName} ... ");
-            StreamReader sr = new StreamReader(todoFileName);
-            int numRead = 0;
-
-            string line;
-            while ((line = sr.ReadLine()) != null)
-            {
-                TodoItem item = new TodoItem(line);
-                list.Add(item);
-                numRead++;
-            }
-            sr.Close();
-            Console.WriteLine($"Läste {numRead} rader.");
-        }
-        public static void TrySetStatus(string arg, int status)
-        {
-            try
-            {
-                int i = Int32.Parse(arg);
-                try
-                {
-                    if (list[i].SetStatus(status)) 
-                        switch(status)
-                        {
-                            case Todo.TodoItem.Active: Console.WriteLine("=> aktiverades!"); break;
-                            case Todo.TodoItem.Ready: Console.WriteLine("=> sattes klar!"); break;
-                            case Todo.TodoItem.Waiting: Console.WriteLine("=> sattes väntande!"); break;
-                            default: Console.WriteLine("Fel: ogilitg status"); break;
-                        }
-                    else
-                        Console.WriteLine("Fel: misslyckades på grund av nuvarande status");
-                }
-                catch (ArgumentOutOfRangeException)
-                {
-                    Console.WriteLine("Uppgiften fanns inte!");
-                }
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Felaktigt nummer!");
-            }
-        }
-        private static void PrintHeadOrFoot(bool head, bool verbose)
-        {
-            if (head)
-            {
-                Console.Write("|# |status      |prio  |namn                |");
-                if (verbose) Console.WriteLine("beskrivning                             |");
-                else Console.WriteLine();
-            }
-            Console.Write("|--|------------|------|--------------------|");
-            if (verbose) Console.WriteLine("----------------------------------------|");
-            else Console.WriteLine();
-        }
-        private static void PrintHead(bool verbose)
-        {
-            PrintHeadOrFoot(head: true, verbose);
-        }
-        private static void PrintFoot(bool verbose)
-        {
-            PrintHeadOrFoot(head: false, verbose);
-        }
-        public static void PrintTodoList(bool verbose = false)
-        {
-            PrintHead(verbose);
-            int i = 0;
-            foreach (TodoItem item in list)
-            {
-                Console.WriteLine($"|{i++,-2}{item.ToString(verbose)}");
-            }
-            PrintFoot(verbose);
-        }
-        public static void PrintHelp()
-        {
-            Console.WriteLine("Kommandon:");
-            Console.WriteLine("hjälp           skriv ut denna hjälp");
-            Console.WriteLine("aktivera /num/  aktivera uppgift (om väntande)");
-            Console.WriteLine("klar /num/      sätt uppgift klar (om aktiv)");
-            Console.WriteLine("lista           lista att-göra-listan");
-            Console.WriteLine("lista allt      lista att-göra-listan utförligt");
-            Console.WriteLine("vänta /num/     sätt uppgift väntande (om aktiv)");
-            Console.WriteLine("sluta           avsluta programmet");
+            Console.WriteLine($"- {item.Key}: {item.Value}");
         }
     }
-    public class MyIO
+}
+
+public class Program
+{
+    public static void Main(string[] args)
     {
-        static public string Argument(string rawCommand, int num)
+        Console.WriteLine("Välkommen till att-göra-listan!");
+        Todo.ReadListFromFile();
+        Todo.PrintHelp();
+        string command;
+
+        do
         {
-            if (NumArguments(rawCommand) < num)
+            command = MyIO.ReadCommand("> ").Trim();
+
+            if (MyIO.CommandEquals(command, "hjälp"))
             {
-                throw new ArgumentException($"Too few arguments to get #{num}!");
+                Todo.PrintHelp();
             }
-            string command = rawCommand.Trim();
-            if (command == "") throw new ArgumentException($"Too few arguments to get #{num}!");
-            string[] cwords = command.Split(' ');
-            return cwords[num + 1];
-        }
-        static public bool Equals(string rawCommand, string expected)
-        {
-            string command = rawCommand.Trim();
-            if (command == "") return false;
+            else if (MyIO.CommandEquals(command, "aktivera"))
+            {
+                Todo.TrySetStatus(MyIO.Argument(command, 1), Todo.TodoItem.Active);
+            }
+            else if (MyIO.CommandEquals(command, "klar"))
+            {
+                Todo.TrySetStatus(MyIO.Argument(command, 1), Todo.TodoItem.Ready);
+            }
+            else if (MyIO.CommandEquals(command, "vänta"))
+            {
+                Todo.TrySetStatus(MyIO.Argument(command, 1), Todo.TodoItem.Waiting);
+            }
+            else if (MyIO.CommandEquals(command, "lista"))
+            {
+                Todo.PrintTodoList(MyIO.HasArgument(command, "allt"));
+            }
+            else if (MyIO.CommandEquals(command, "sluta"))
+            {
+                Console.WriteLine("Hej då!");
+                break;
+            }
             else
             {
-                string[] cwords = command.Split(' ');
-                if (cwords[0] == expected) return true;
+                Console.WriteLine($"Okänt kommando: {command}");
             }
-            return false;
         }
-        static public bool HasArgument(string rawCommand, string expected)
-        {
-            string command = rawCommand.Trim();
-            if (command == "") return false;
-            else
-            {
-                string[] cwords = command.Split(' ');
-                if (cwords.Length < 2) return false;
-                if (cwords[1] == expected) return true;
-            }
-            return false;
-        }
-        static public int NumArguments(string rawCommand)
-        {
-            string command = rawCommand.Trim();
-            if (command == "") return 0;
-            string[] cwords = command.Split(' ');
-            return cwords.Length - 1;
-        }
-        static public string ReadCommand(string prompt)
-        {
-            Console.Write(prompt);
-            return Console.ReadLine();
-        }
+        while (true);
     }
 }
